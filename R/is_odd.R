0691fca168a30bf798946dc66a083919a6a4b476
Frederic FOUCAULT <frederic.foucault@firalis.com>
2023-11-20 15:14:34 +0100

    first commit

---
+++ R/is_odd.R
@@ -0,0 +1,16 @@
+#' @title Check whether a number is odd
+#'
+#' @param x numeric vector
+#'
+#' @returns A logical vector that calculate the modulo to zero if number x is odd or not
+#'
+#' @examples
+#' is_odd(3)
+#' is_odd(2)
+#'
+#'
+#'
+#' @export
+is_odd <- function(x){
+  x %%2 ==0
+}
