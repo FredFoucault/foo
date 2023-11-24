3784a25c61fcfb3334746a3bd4bfa3368343d72f
FredFoucault <frederic.foucault@firalis.com>
2023-11-24 18:17:50 +0100

    format tibble

---
+++ R/expression.R
@@ -0,0 +1,12 @@
+#'
+#' Data gene expression
+#'
+#'
+#'
+#' @format a tibble
+#'
+#'  \describe{
+#'  \item{gene} {gene name}
+#'  \item{sample} {sample anme}
+#'  \item{count} {counts of exprression}
+#'  }
